06e4a875058d6234fc011570339f9c9c7e9be66a
Prxshxnt <prashant.chauhan902k@gmail.com>
2026-07-20 16:31:33 +0530

    add AppointmentService

---
+++ src/main/java/com/Hospital/hospitalManagementSystem/service/AppointmentService.java
@@ -0,0 +1,11 @@
+package com.Hospital.hospitalManagementSystem.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+
+
+}
